6be9efe11422d8fdcfd645f724d19a3edacff954
nico <nico.forni@hotmail.com>
2022-02-02 01:12:52 -0300

    nav-bar

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -2,17 +2,22 @@ import "./Navbar.css"
 
 const Navbar = () => {
     return(
-      <nav>
+        <nav>
         <ul className="nav-menu">
-          <img className="logo" src="./assets/logo-pc.svg" alt="" width="" height=""></img>
-          <div className="texto-de-menu">
-              <li>Inicio</li>
-              <li>Productos</li>
-              <li>Nosotros</li>
-              <li>Contacto</li>                        
-          </div>
+            <img className="logo" src="./images/logo-cine.svg" alt="" width="" height=""></img>
+            <div className="texto-de-menu">
+              <li><a href="#">Peliculas</a></li>
+              <li><a href="#">Series</a></li>
+              <li><a href="#">Documentales</a></li>
+              <li><a href="#">Novedades</a></li>                                      
+            </div>
+            <input className="buscadorcito" id="buscador" type="text" placeholder="Escriba un film"></input>
+            <button className="botoncito" id="boton" type="submit">Buscar...</button>
+            {/* <div class="carrito-contenedor">
+                <a href="#modal" rel="modal:open"></a><img src="./images/carrito.png" alt="Carrito Icono"></img>
+            </div>              */}
         </ul>
-      </nav>
+        </nav>
     )
 }
 
