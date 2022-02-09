835d52e5396b83865738fc3a9dea2e5e0976507a
nico <nico.forni@hotmail.com>
2022-02-09 14:11:04 -0300

    boton con contador

--- src/Components/CartWidget/Cartwidget.js
+++ src/Components/CartWidget/Cartwidget.js
@@ -2,7 +2,7 @@ import "./Cartwidget.css"
 
 const Cartwidget = () => {
     return(
-        <div class="carrito-contenedor">
+        <div className="carrito-contenedor">
         <a href="#modal" rel="modal:open"><img src={"./images/carrito.png"} alt={"Carrito Icono"}></img>4</a>
         </div>
     )
