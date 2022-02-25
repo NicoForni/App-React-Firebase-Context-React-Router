29da90a777004b3d3f118c79070d6fe8c08b5558
nico <nico.forni@hotmail.com>
2022-02-25 00:15:36 -0300

    actualizado

--- src/Components/CartWidget/Cartwidget.js
+++ src/Components/CartWidget/Cartwidget.js
@@ -3,9 +3,9 @@ import "./Cartwidget.css"
 
 const Cartwidget = () => {
     return(
-        <div className="carrito-contenedor">
-        <a href="#modal" rel="modal:open"><img src={"./images/carrito.png"} alt={"Carrito Icono"}></img>4</a>
-        </div>
+        
+        <div className="carrito-contenedor"><img src={"./images/carrito.png"} alt={"Carrito Icono"}></img>4</div>
+    
     )
 }
 
